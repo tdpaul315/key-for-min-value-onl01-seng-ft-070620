7f3fd1b53c0372d8700aac2fd8a3d84ec930be46
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 05:47:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+network_ratings = {abc => "75", mtv => "87", fox => "100"}
 
-def key_for_min_value(name_hash)
+def key_for_min_value(network_ratings)
+  network_ratings.collect 
 
 end
