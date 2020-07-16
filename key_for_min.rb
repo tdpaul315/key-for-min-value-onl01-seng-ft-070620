cd14c21544e6bb275855f04df64b28eb9e7761f3
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 05:56:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
+ikea = {:chair => 25, :table => 85, :mattress => 450}
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   lowest_value = 0
