1fb7cc63cf24575e281475cea1a10ff78f2285c4
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 05:53:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,2 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-network_ratings = {abc => "75", mtv => "87", fox => "100"}
-
-def key_for_min_value(network_ratings)
-  network_ratings.collect do |network,rating|
-    if rating <= "85" || rating >= "85"
-    end 
-
-end
